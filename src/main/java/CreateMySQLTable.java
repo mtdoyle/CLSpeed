90ae17ea3369fb0bb0289d3f33d839ffd12393d8
Mike Doyle <mike.doyle@thomsonreuters.com>
2016-03-21 15:44:09 -0500

    Switching speed column to decimal

--- src/main/java/CreateMySQLTable.java
+++ src/main/java/CreateMySQLTable.java
@@ -1,8 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 /**
  * Created by mike on 3/16/16.
